788a58b5eb561f60a8fda33127b0f893f4b08de4
Isuru <isurusajith0@gmail.com>
2023-08-15 11:06:32 +0530

    sum with for loop done

--- src/main/java/lk/sp/solutions/test/Answer1.java
+++ src/main/java/lk/sp/solutions/test/Answer1.java
@@ -3,5 +3,13 @@ package lk.sp.solutions.test;
 public class Answer1 {
     public static void main(String[] args) {
 
+        int[] numbers = new int[]{10, 50, 6, 55, 4, 8, 32};
+        int sum=0;
+
+
+        for (int i = 0; i < numbers.length; i++) {
+            sum += numbers[i];
+        }
+        System.out.println(sum);
     }
 }
